058261470d410b7e07735dd231e6cd81dc8caf2b
Fedekh <federicocet@gmail.com>
2023-04-05 14:47:23 +0200

    add scaffolding easter's exercises

---
+++ Esercizio password-generator/Milestone 1/js/script.js
@@ -0,0 +1,17 @@
+// Descrizione
+// Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
+
+// Milestone 1
+// Creare un input dove l'utente inserisce la lunghezza della password. Al click sull'invia,
+//  l'app genera una password casuale (composta da lettere, lettere maiuscole, numeri e simboli). La password generata viene visulizzata nella pagina.
+
+
+const { createApp } = Vue;
+
+  createApp({
+    data() {
+      return {
+        
+    },
+    }
+  }).mount('#app');
