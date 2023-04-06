5b2b8af1a68ab748ac9137024ac9b4c31ecf0815
Fedekh <federicocet@gmail.com>
2023-04-06 17:15:33 +0200

    per ora si puo scegliere se solo numerica o alfabetica o simboli o combinazioni tra loro

--- Esercizio password-generator/Milestone 2/js/script.js
+++ Esercizio password-generator/Milestone 2/js/script.js
@@ -1,26 +1,106 @@
-// Descrizione
-// Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
-
-// Milestone 1
-// Creare un input dove l'utente inserisce la lunghezza della password. Al click sull'invia,
-//  l'app genera una password casuale (composta da lettere, lettere maiuscole, numeri e simboli).
-// La password generata viene visulizzata nella pagina.
-
+// Milestone 2
+// Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli.
+//  Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
+// Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali.
 
 const { createApp } = Vue;
 
-  createApp({
-    data() {
-      return {
-        lunghezzaPassword:"",
-        passwordGenerata:[],prova
+createApp({
+  data() {
+    return {
+      lunghezzaPassword: "",
+      passwordGenerata: "",
+      valido: false,          // flag per vede se l input è valido o meno
+      si: false,
+      no: false,             //collegati alle scelte si  o no
+      lettere: false,
+      numeri: false,
+      simboli: false,         //collegati alla tipologia di password
+      isChecked: null
     }
-    },
-    methods:{
-      generaPassword(){
-        //la lunghezza della password è lunghezzaPassword
-        //occorre
-    }
-  }
+  },
 
-  }).mount('#app');s
+  methods: {
+    click(scelta) {    //mi seleziona o deseleziona i checkbox
+      this.scelta = !this.scelta
+    },
+
+    generaNumeroRandomico() {
+      let numero = Math.floor(Math.random() * 10);
+      let lettera = Math.floor(Math.random() * 1) < .5 ? Math.floor(Math.random() * (122 - 97 + 1)) + 97 : Math.floor(Math.random() * (90 - 65 + 1)) + 65;
+      let simbolo = Math.floor(Math.random() * (64 - 33)) + 33;
+
+      if (this.numeri && !this.lettere && !this.simboli) {
+        console.log(numero);
+        return numero
+      } else if (this.lettere && !this.numeri && !this.simboli) {
+        console.log(lettera);
+        return lettera
+      } else if (this.simboli && !this.numeri && !this.lettere) {
+        console.log(simbolo);
+        return simbolo
+      } else if (this.simboli && this.numeri && !this.lettere) {
+        return Math.random() < 0.5 ? numero : simbolo;
+      } else if (this.simboli && this.lettere && !this.numeri) {
+        return Math.random() < 0.5 ? lettera : simbolo;
+      } else if (this.numeri && this.lettere && !this.simboli) {
+        return Math.random() < 0.5 ? lettera : numero;
+      } else {
+        let casual = Math.floor(Math.random() * 3);  //un numero casuale da 1 a 3 x decidere quale pushare
+        if (casual === 1) {
+          return numero
+        } else if (casual === 2) {
+          return simbolo
+        } else {
+          return lettera
+        }
+      }
+      // return Math.random() < 0.5 ? numero : randomUnicode;
+    },
+
+    generaPassword() {
+      this.passwordGenerata = "";   //reset a priori del campo input
+      if (this.lunghezzaPassword === '' ||
+        isNaN(this.lunghezzaPassword) ||
+        this.lunghezzaPassword > 10 ||
+        this.lunghezzaPassword < 4 ||
+        !this.lettere ||
+        !this.numeri ||
+        !this.simboli) {
+        this.valido = true;        //compare subito errore,ma dopo 5 secondi scompare con riga 37
+        setTimeout(() => {
+          this.valido = false;
+        }, 5000);
+        this.lunghezzaPassword = ""; //reset input
+      } else {
+        for (let i = 0; i < this.lunghezzaPassword; i++) {
+          const valoreUnicode = this.generaNumeroRandomico();
+          if (valoreUnicode < 10) {
+            this.passwordGenerata += valoreUnicode;
+          } else {
+            this.passwordGenerata += String.fromCharCode(valoreUnicode);
+          }
+        }
+        console.log(`La password generata è : ${this.passwordGenerata}`);
+        this.passwordGenerata = this.passwordGenerata.join("");   //levo ogni spazio tra i caratteri della stringa
+        this.lunghezzaPassword = "";                              //reset input
+      }
+      // reset();
+    },
+
+
+    // azzeriamo tutti i campi
+    reset() {
+      this.lunghezzaPassword = "";
+      this.passwordGenerata = "";
+      this.lettere = false;
+      this.numeri = false;
+      this.simboli = false;
+      this.isChecked = false;
+      if (this.valido = false) {
+        this.valido = true;
+      }
+    }
+
+  }
+}).mount('#app');
